9de637a74ddfffb1cbb78da1246733df1b48ab83
David Parry <drjparry@gmail.com>
2016-03-09 19:18:06 +0100

    completed first user story

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,23 @@
-githubCreep.controller('userListCtrl', function($scope) {
-  $scope.users = [
-  {'name': 'drjparry' }
-  ]
-})0
+
+githubCreep.controller('userListCtrl', ['$scope', '$http',
+
+
+  function($scope, $http) {
+
+    $scope.searchUser=function(){
+      $http.get('https://api.github.com/search/users?q='+$scope.query).success(function(data) {
+      $scope.users = data.items;
+    });
+  };
+
+}])
+
+
+// .controller('gitHubDataController', ['$scope','$http', function($scope,$http) {
+
+// $scope.username = "pdsullivan";
+// $http.get("https://api.github.com/users/"+$scope.username)
+//         .success(function(data) {
+//             $scope.userData = data;
+//         });
+// }]);
--- test/e2e-tests/scenarios.js
+++ test/e2e-tests/scenarios.js
@@ -13,8 +13,11 @@ describe('GithubCreep', function() {
     it('should display a list of github users', function() {
         var userList = element.all(by.repeater('user in users'));
         var query = element(by.model('query'));
+        var button = element(by.id('button'));
 
         query.sendKeys('drjparry');
+        button.click();
+
         expect(userList.count()).toBe(1);
     });
 
