40e59aa8ca3174bd855fba8cf11ffee0b96b7132
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-03-10 19:32:02 +0100

    WIP unit test search by username

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,11 +4,12 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js',
+      'app/js/app.js',
+      'app/js/controllers.js',
       'test/unit/controllersSpec.js'
     ],
 
